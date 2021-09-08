368cf98880e84afbc80802fb54909602fb89d4b5
moveuk <v_donguk@naver.com>
2021-09-08 20:13:05 +0900

    0.1.2v music 업데이트 및 db context 파일 추가

---
+++ src/main/java/com/indie/controller/action/music/IndieMusicAction.java
@@ -0,0 +1,19 @@
+package com.indie.controller.action.music;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.indie.controller.action.Action;
+
+public class IndieMusicAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+
+}
